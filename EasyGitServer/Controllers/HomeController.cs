2cb1ecbc69e8eae2861d81d67221d09503704f3c
lioborsvoboda <libor.svoboda@kliknetezde.cz>
2026-03-31 15:13:15 +0200

    First Upload

---
+++ EasyGitServer/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using System.Linq;
+using GitServer.Services;
+using GitServer.Settings;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authorization;
+using GitServer.ApplicationCore.Interfaces;
+using GitServer.ApplicationCore.Models;
+using System.IO;
+using System;
+
+namespace GitServer.Controllers
+{
+    [Authorize]
+    public class HomeController : GitControllerBase
+    {
+        private IGitDbRepository<Repository> _repository;
+
+        public HomeController(
+            IOptions<GitSettings> gitOptions,
+            GitRepositoryService repoService,
+            IGitDbRepository<Repository> repository
+        )
+            : base(gitOptions, repoService)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Home()
+        {
+            var username = HttpContext.User.Identity.Name;
+            var reps = _repository.List(r => r.UserName == username).ToList();
+            return View(reps);
+        }
+
+        public IActionResult Shared() {
+            var username = HttpContext.User.Identity.Name;
+            var reps = _repository.List(r => r.IsPrivate == false).ToList();
+            return View(reps);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string name, string remoteurl, string description)
+        {
+            LibGit2Sharp.Repository result = null;
+            name = name.Trim();
+            var username = HttpContext.User.Identity.Name;
+            var reps = _repository.List(r => r.UserName == username).ToList();
+            if (reps.Count > 9)
+                return View(new { error = "ŇŃł¬ąý10¸öĎŢÖĆ" });
+            if (reps.Exists(r => r.Name == name))
+                return View(new { error = "ŇŃ´ćÔÚ˛Öżâ" });
+            if (!string.IsNullOrEmpty(name) && string.IsNullOrEmpty(remoteurl))
+            {
+                result = RepositoryService.CreateRepository(Path.Combine(username, name));
+            }
+            else if (!string.IsNullOrEmpty(remoteurl))
+            {
+                remoteurl = remoteurl.Trim();
+                result = RepositoryService.CreateRepository(Path.Combine(username, name), remoteurl);
+            }
+            if (result != null)
+            {
+                var rep = new Repository()
+                {
+                    Name = name,
+                    Description = description,
+                    CreationDate = DateTime.Now,
+                    DefaultBranch = "master",
+                    UserName = username,
+                    UpdateTime = DateTime.Now
+                };
+                _repository.Add(rep);
+                return Redirect("/");
+            }
+            return View();
+        }
+    }
+}
---
+++ EasyGitServer/RouteConfig.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace GitServer
+{
+	public static class RouteConfig
+    {
+		public static void RegisterRoutes(IRouteBuilder routeBuilder)
+		{
+
+            routeBuilder.MapRoute(
+                "Home",
+                "{controller=Home}/{action=Home}/{id?}"
+            );
+
+            //routeBuilder.MapRoute(
+            //    "User",
+            //    "{controller=User}/{action=Login}"
+            //);
+
+            #region Routes for viewing the file tree
+            routeBuilder.MapRoute(
+                "RedirectGitLink",
+                "{userName}/{repoName}.git",
+                new { controller = "FileView", action = "RedirectGitLink" },
+                new { method = new HttpMethodRouteConstraint("GET") }
+            );
+
+            routeBuilder.MapRoute(
+                "GetRepositoryHomeView",
+                "{userName}/{repoName}",
+                new { controller = "FileView", action = "GetTreeView", id = "master", path = string.Empty },
+                new { method = new HttpMethodRouteConstraint("GET") }
+            );
+
+            routeBuilder.MapRoute(
+                "GetTreeView",
+                "{userName}/{repoName}/tree/{id}/{*path}",
+                new { controller = "FileView", action = "GetTreeView" },
+                new { method = new HttpMethodRouteConstraint("GET") }
+            );
+
+            routeBuilder.MapRoute(
+                "GetBlobView",
+                "{userName}/{repoName}/blob/{id}/{*path}",
+                new { controller = "FileView", action = "GetBlobView" },
+                new { method = new HttpMethodRouteConstraint("GET") }
+            );
+
+            routeBuilder.MapRoute(
+                "GetRawBlob",
+                "{userName}/{repoName}/raw/{id}/{*path}",
+                new { controller = "FileView", action = "GetRawBlob" },
+                new { method = new HttpMethodRouteConstraint("GET") }
+            );
+            #endregion
+        }
+    }
+}
